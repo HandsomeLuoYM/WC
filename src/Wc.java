abbe4f45415b02fa0774c1b64282f5efa965b41b
LuoYM-Warehouse <git2224357949@qq.com>
2020-03-13 15:32:49 +0800

    3/13 实现基本方法

--- src/Wc.java
+++ src/Wc.java
@@ -8,24 +8,69 @@ import java.util.Scanner;
  */
 public class Wc {
 
+    //拆分单词的正则
+    private static final String SPILT_LINE_TO_WORD = "\\s+|\\(|\\)|,|\\.|\\:|\\{|\\}|\\-|\\+|;|\\?|\\/|\\\\|/";
     private static final String WORD_FORMAT = "[a-zA-Z]";
 
-//    public static int
+    /**
+     * 判定用户操作
+     * @return
+     * @throws IOException
+     */
+    public static void basicOperation(String[] split) throws IOException {
+
+        //初始化统计数据参数
+        int characterCount = 0;
+        int lineCount = 0;
+        int wordCount = 0;
+
+        //获取本地文件的缓存流读取器，方便后面操作
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(split[2])));
+        //用于暂时存储行数据
+        String line = new String();
+        switch (split[1]){
+            case "-c" :
+                //计算字符长度
+                while (null != (line = br.readLine())){
+                    characterCount +=  line.length();
+                }
+                System.out.println("字符数为：" + characterCount);
+                break;
+            case "-l" :
+                while (br.readLine() != null){
+                    lineCount++;
+                }
+                System.out.println("行数为：" + lineCount);
+                break;
+            case "-w" :
+                String[] words;
+                while ((line = br.readLine()) != null){
+                    words = line.split(SPILT_LINE_TO_WORD);
+                    wordCount += words.length;
+                }
+                System.out.println("单词数："+wordCount);
+                break;
+            default:
+                System.out.println("输入错误！");
+        }
+
+
+    }
 
     public static void main(String[] args) throws IOException {
-        //获取用户输入值
-//        Scanner scanner = new Scanner(System.in);
-//        String command = scanner.nextLine();
-        File file = new File("D:\\idea_maven\\WC\\src\\a.c");
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-        int characterNumber = 0;
-        String line = new String();
-        while (null != (line = br.readLine())){
-            characterNumber = characterNumber + line.length();
-            System.out.println(line );
-        }
-        System.out.println(characterNumber);
+        //获取用户输入指令
+        Scanner scanner = new Scanner(System.in);
+        //循环来获取用户操作
+        while (true){
+            //将指令集拆分
+            String[] split = scanner.nextLine().split(" ");
 
+            if("wc.exe".equals(split[0])){
+                basicOperation(split);
+            }else {
+                System.out.println("Error：输入的指令错误");
+            }
+        }
 //        if (file.isDirectory()){
 //            String[] list = file.list();
 //            for(String name:list){
